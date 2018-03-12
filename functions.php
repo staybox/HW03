01b5cb7d1e2d8e3ac7a6fee23971150333523f84
Andrey <staybox@gmail.com>
2018-03-12 12:18:35 +0300

    ДЗ3

--- functions.php
+++ functions.php
@@ -6,36 +6,37 @@
  * Time: 0:30
  */
 
-function task1 (){
+function task1()
+{
     if (file_exists('data.xml')) {
         $xml = simplexml_load_file('data.xml');
         echo "Номер заказа: " . $xml->attributes()['PurchaseOrderNumber'] . "\n";
         echo "Дата заказа: " . $xml->attributes()['OrderDate'] . "\n";
         foreach ($xml->Address as $items) {
-            echo "--------------"."\n";
+            echo "--------------" . "\n";
             if ($items->attributes() == "Shipping") {
-                echo "Адрес доставки: " ."\n";
-            }elseif ($items->attributes() == "Billing"){
-                echo "Адрес выставления счёта: " ."\n";
+                echo "Адрес доставки: " . "\n";
+            } elseif ($items->attributes() == "Billing") {
+                echo "Адрес выставления счёта: " . "\n";
             }
-            echo "ФИО: " . $items->Name. "\n";
-            echo "Страна: " . $items->Country. "\n";
-            echo "Штат: " . $items->State. "\n";
-            echo "Город: " . $items->City. "\n";
-            echo "Индекс: " . $items->Zip. "\n";
-            echo "Улица: " . $items->Street. "\n";
+            echo "ФИО: " . $items->Name . "\n";
+            echo "Страна: " . $items->Country . "\n";
+            echo "Штат: " . $items->State . "\n";
+            echo "Город: " . $items->City . "\n";
+            echo "Индекс: " . $items->Zip . "\n";
+            echo "Улица: " . $items->Street . "\n";
         }
         echo "\n";
         echo "Комментарии к заказу: " . $xml->DeliveryNotes . "\n";
         foreach ($xml->Items->Item as $item) {
-            echo "--------------"."\n";
+            echo "--------------" . "\n";
             echo "Артикул товара: " . $item->attributes() . "\n";
             echo "Название товара: " . $item->ProductName . "\n";
             echo "Количество: " . $item->Quantity . "\n";
-            echo "Стоимость: " . $item->USPrice . "$" ."\n";
-            if($item->Comment != ''){
+            echo "Стоимость: " . $item->USPrice . "$" . "\n";
+            if ($item->Comment != '') {
                 echo "Комментарий: " . $item->Comment . "\n";
-            }elseif ($item->ShipDate != ''){
+            } elseif ($item->ShipDate != '') {
                 echo "День доставки: " . $item->ShipDate . "\n";
             }
         }
@@ -44,7 +45,8 @@ function task1 (){
     }
 }
 
-function task2($arr){
+function task2($arr)
+{
     // Преобразование в JSON и запись в первый файл
     $en = json_encode($arr);
     file_put_contents('output.json', $en);
@@ -54,17 +56,17 @@ function task2($arr){
     $dataOne = json_decode($jsonString, true);
 
     // Меняем данные в массиве и записываем их во второй файл
-    $dataTwoReplace = [['a' => 66, 'b' => 2, 'c' => 3, 'd' => 4, 'e' => 5],['q' => 88, 's' => 25, 'z' => 33, 't' => 49, 'o' => 56]];
-    $basket = array_replace($dataOne,$dataTwoReplace);
+    $dataTwoReplace = [['a' => 66, 'b' => 2, 'c' => 3, 'd' => 4, 'e' => 5], ['q' => 88, 's' => 25, 'z' => 33, 't' => 49, 'o' => 56]];
+    $basket = array_replace($dataOne, $dataTwoReplace);
     $newJsonString = json_encode($basket);
     file_put_contents('output2.json', $newJsonString);
 
     // Читаем второй файл
     $get = file_get_contents('output2.json');
-    $dataTwo = json_decode($get,true);
+    $dataTwo = json_decode($get, true);
 
     // Проверяем расхождения массивов
-    for($i=0;$i<2;$i++) {
+    for ($i = 0; $i < 2; $i++) {
         $result1 = array_diff($dataOne[$i], $dataTwo[$i]);
         $result2 = array_diff($dataTwo[$i], $dataOne[$i]);
         print_r($result1);
@@ -72,10 +74,11 @@ function task2($arr){
     }
 }
 
-function task3 (){
+function task3()
+{
     // Создание массива из случайных чисел
-    for($i=0; $i<50; $i++) {
-        $arr[$i] = rand(1,100);
+    for ($i = 0; $i < 50; $i++) {
+        $arr[$i] = rand(1, 100);
     }
     $arrayTwo[] = $arr;
 
@@ -87,13 +90,12 @@ function task3 (){
     }
 
     fclose($fp);
+    $csv = array_map('str_getcsv', file('task3.csv'));
     $sum = 0;
-    for($i=0;$i<count($arr);$i++)
-    {
-        if($arr[$i] % 2==0)
-        {
-            echo $arr[$i].' - четное.' . "\n";
-            $sum += $arr[$i];
+    for ($i = 0; $i < count($csv[0]); $i++) {
+        if ($csv[0][$i] % 2 == 0) {
+            echo $csv[0][$i] . ' - четное.' . "\n";
+            $sum += $csv[0][$i];
             //echo "Сумма: " . $sum . "\n";
 
         }
@@ -106,7 +108,7 @@ function task4()
     $url = "https://en.wikipedia.org/w/api.php?action=query&titles=Main%20Page&prop=revisions&rvprop=content&format=json";
     $content = file_get_contents($url);
     $json = json_decode($content, true);
-    print_r($json["query"]["pages"]["15580374"]["title"]. "\n");
+    print_r($json["query"]["pages"]["15580374"]["title"] . "\n");
     print_r($json["query"]["pages"]["15580374"]["pageid"]);
 
     //  2. Вывести title и page_id
@@ -115,4 +117,3 @@ function task4()
     //    var_dump($value); //$value . "\n";
     //}
 }
-
